587051f2dd5335e2b2653b61a2473826d4e31c20
pat-s <patrick.schratz@gmail.com>
2019-08-06 17:14:53 +0200

    upd tic.R

--- tic.R
+++ tic.R
@@ -9,15 +9,20 @@ get_stage("deploy") %>%
   add_step(step_add_to_known_hosts("github.com")) %>%
   add_step(step_install_ssh_keys()) %>%
   add_step(step_test_ssh()) %>%
-  add_step(step_setup_push_deploy(path = "docs", branch = "gh-pages")) %>%
+  add_step(step_setup_push_deploy(branch = "gh-pages", orphan = TRUE)) %>%
+
+  # delete old contents if they exist
+  add_step(step_run_code(file.remove("courses/basis/eds-test/*"))) %>%
 
   add_step(step_run_code(source("create-course.R"))) %>%
   add_step(step_run_code(source("basis-course.R"))) %>%
   add_step(step_run_code(source("courses/basis/2019_03/render-course.R"))) %>%
   add_step(step_run_code(rmarkdown::render_site("courses/basis/2019_03"))) %>%
-  add_step(step_run_code(encrypt_html_file("courses/basis/eds-test/basis_descriptive.html",
-                                           output_path = "courses/basis/eds-test/basis_descriptive_enc.html")))
+  # encrypt course html file
+  add_step(step_run_code(encryptedRmd::encrypt_html_file("courses/basis/eds-test/basis_descriptive.html",
+                                                         output_path = "courses/basis/eds-test/basis_descriptive.html",
+                                                         write_key_file = TRUE))) %>%
 
+  # finally push everything
   add_step(step_run_code(file.copy(dir("courses/basis/eds-test/", full.names = TRUE), "docs/", recursive = TRUE))) %>%
-  #add_step(step_run_code(file.copy("courses/basis/2019_03/basis_descriptive.html", "docs", overwrite = T))) %>%
   add_step(step_do_push_deploy(path = "docs"))
